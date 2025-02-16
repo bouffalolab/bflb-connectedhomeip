0e86f3f086e1cdb32dacaf30a537e41e4cb91314
wyhong <30567533+wy-hh@users.noreply.github.com>
2025-02-16 14:48:45 +0800

    [bouffalo lab] Add pbuf_ram support for lwip application and add bouffalo sdk submodule (#3)
    * [Bouffalo Lab] Add a script to demonstrate mfd partition generation (#35192)
    
    * [Bouffalo Lab] Add a script to demonstrate mfd partition generation
    
    * fix restyle
    
    * fix restyle
    
    * fix lint code and spell
    
    * fix lint code and spell
    
    * revert changes on Makefile of doc
    
    * [bouffalo lab] fix crash issues in route hook and rpc uart driver mod… (#36944)
    
    * [bouffalo lab] fix crash issues in route hook and rpc uart driver modules
    
    * Restyled by clang-format
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * [bouffalo lab] add bl616 lighting example with Wi-Fi and Thread suppo… (#36954)
    
    * [bouffalo lab] add bl616 lighting example with Wi-Fi and Thread supported
    
    * Restyled by gn
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Andrei Litvin <andy314@gmail.com>
    
    * [bouffalo lab] add bl704l thread sed, low power, support on contact s… (#36953)
    
    * [bouffalo lab] add bl704l thread sed, low power, support on contact sensor example
    
    * update zap file
    
    * [bouffalo lab] Update Bouffalo Lab platform documents (#37067)
    
    * [bouffalo lab] Update bouffalo lab documents for contact sensor and new platform bl616
    
    * Restyled by prettier-markdown
    
    * fix document references
    
    * Restyled by prettier-markdown
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * change contact sensor zap to version 103
    
    * add pbuf ram support on matter
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Andrei Litvin <andy314@gmail.com>

--- scripts/checkout_submodules.py
+++ scripts/checkout_submodules.py
@@ -28,7 +28,6 @@ ALL_PLATFORMS = set([
     'ameba',
     'android',
     'asr',
-    'bl602',
     'bouffalolab',
     'cc13xx_26xx',
     'cc32xx',
@@ -51,6 +50,7 @@ ALL_PLATFORMS = set([
     'genio',
     'openiotsdk',
     'silabs_docker',
+    'bouffalo_sdk'
 ])
 
 Module = namedtuple('Module', 'name path platforms recursive')
