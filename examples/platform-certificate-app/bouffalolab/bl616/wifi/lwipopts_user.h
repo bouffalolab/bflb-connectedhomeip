2ae4ff1ee1154cbbdcd8fbf963ce159192e2c607
wyhong <wyhong@bouffalolab.com>
2026-04-16 18:02:17 +0800

    enable igmp and update enable key

--- examples/platform-certificate-app/bouffalolab/bl616/wifi/lwipopts_user.h
+++ examples/platform-certificate-app/bouffalolab/bl616/wifi/lwipopts_user.h
@@ -121,7 +121,7 @@
 
 #define LWIP_DHCP 1
 #define LWIP_DNS 1
-#define LWIP_IGMP 0
+#define LWIP_IGMP 1
 #define LWIP_SO_RCVTIMEO 1
 #define LWIP_SO_SNDTIMEO 1
 #define SO_REUSE 1
--- examples/platform/bouffalolab/common/plat/platform.cpp
+++ examples/platform/bouffalolab/common/plat/platform.cpp
@@ -108,9 +108,9 @@ FactoryDataProvider sFactoryDataProvider;
 
 #if CONFIG_TEST_EVENT_TRIGGER_ENABLED
 namespace {
-static uint8_t sTestEventTriggerEnableKey[TestEventTriggerDelegate::kEnableKeyLength] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55,
-                                                                                          0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb,
-                                                                                          0xcc, 0xdd, 0xee, 0xff };
+static uint8_t sTestEventTriggerEnableKey[TestEventTriggerDelegate::kEnableKeyLength] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05,
+                                                                                          0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b,
+                                                                                          0x0c, 0x0d, 0x0e, 0x0f };
 }
 #endif
 
